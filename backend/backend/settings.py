4c979dc9a1a319c685b34f398df71377f835f6c1
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2023-07-18 14:20:06 +0100

    remove pyc files and update db settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'something',
+        'USER': 'admin',
+        'PASSWORD': 'docprocessor',
+        'HOST': 'courses-db.czlgzrfrhsu6.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
