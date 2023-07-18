e546654d3c31a33e79fda4ccd8f0dd14c4ef839e
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2023-07-18 14:08:07 +0100

    added theme ui and tsconfig

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import BrowseCourses from "./components/BrowseCourses";
+
+function App() {
+  return <BrowseCourses />;
+}
+
+export default App;
---
+++ frontend/src/components/BrowseCourses.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Box } from "theme-ui";
+
+const BrowseCourses: React.FC = () => {
+  return <Box sx={{}}>Hello World</Box>;
+};
+
+export default BrowseCourses;
