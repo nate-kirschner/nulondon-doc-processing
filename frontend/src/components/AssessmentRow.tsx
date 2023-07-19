f0fe25d4508705d2b6e15c8586a73bae7c1ded0e
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2023-07-19 15:24:45 +0100

    added assignments to ui

---
+++ frontend/src/components/AssessmentRow.tsx
@@ -0,0 +1,93 @@
+import {
+  AccordionDetails,
+  Box,
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
+import Assessment from "../types/assessments";
+import { colors } from "../theme";
+import { useState } from "react";
+
+const AssessmentRow: React.FC<Assessment> = ({
+  title,
+  weighting,
+  duration,
+  length,
+  versions,
+}) => {
+  const versionToString = (version: number): string => {
+    return `v${version}`;
+  };
+
+  const [selectedVersion, setSelectedVersion] = useState(
+    versionToString(versions[versions.length - 1])
+  );
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSelectedVersion(event.target.value);
+  };
+
+  return (
+    <AccordionDetails
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        boxShadow: "0px 0px 3px 0px rgba(0,0,0,0.2)",
+        marginBottom: "16px",
+        borderRadius: "4px",
+      }}
+    >
+      <Typography sx={{ fontSize: "16px" }}>{title}</Typography>
+      <Box sx={{ display: "flex", columnGap: "24px" }}>
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+          <InputLabel id="demo-simple-select-label">Version</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={selectedVersion}
+            label="Version"
+            onChange={handleChange}
+          >
+            {versions.map((version) => {
+              return (
+                <MenuItem value={versionToString(version)}>
+                  {versionToString(version)}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+        <Button
+          sx={{
+            "&:hover": {
+              backgroundColor: "#C8102E4D",
+            },
+            color: colors.black,
+          }}
+          color="secondary"
+        >
+          Export
+        </Button>
+        <Button
+          sx={{
+            "&:hover": {
+              backgroundColor: "#C8102E4D",
+            },
+            color: colors.black,
+          }}
+          color="secondary"
+        >
+          New Version
+        </Button>
+      </Box>
+    </AccordionDetails>
+  );
+};
+
+export default AssessmentRow;
--- frontend/src/components/BrowseCourses.tsx
+++ frontend/src/components/BrowseCourses.tsx
@@ -5,15 +5,51 @@ import usePagination from "../hooks/usePagination";
 import Spacer from "./Spacer";
 import CourseTable from "./CourseTable";
 import useSearchTable from "../hooks/useSearchTable";
+import { colors } from "../theme";
+
+const assessment = { title: "Assessment 1", weighting: 20, versions: [1] };
 
 const BrowseCourses: React.FC = () => {
   const rows: Course[] = [
-    { title: "course 1", code: "IS3500", credits: 4 },
-    { title: "course 2", code: "IS3501", credits: 4 },
-    { title: "course 3", code: "IS3502", credits: 4 },
-    { title: "course 4", code: "IS3503", credits: 4 },
-    { title: "course 5", code: "IS3504", credits: 4 },
-    { title: "course 6", code: "IS3505", credits: 4 },
+    {
+      title: "Course 1",
+      code: "IS3500",
+      credits: 4,
+      assessments: [
+        assessment,
+        { title: "Assessment 2", weighting: 50, versions: [1, 2, 3] },
+      ],
+    },
+    {
+      title: "course 2",
+      code: "IS3501",
+      credits: 4,
+      assessments: [assessment],
+    },
+    {
+      title: "course 3",
+      code: "IS3502",
+      credits: 4,
+      assessments: [assessment],
+    },
+    {
+      title: "course 4",
+      code: "IS3503",
+      credits: 4,
+      assessments: [assessment],
+    },
+    {
+      title: "course 5",
+      code: "IS3504",
+      credits: 4,
+      assessments: [assessment],
+    },
+    {
+      title: "course 6",
+      code: "IS3505",
+      credits: 4,
+      assessments: [assessment],
+    },
   ];
 
   const [filteredRows, setFilteredRows] = useState<Course[]>(rows);
@@ -45,7 +81,19 @@ const BrowseCourses: React.FC = () => {
           value={value}
           onChange={onChange}
           placeholder="Search course titles or codes"
+          sx={{
+            ":before": {
+              borderBottomColor: colors.red,
+            },
+            ":after": {
+              borderBottomColor: colors.red,
+            },
+            "&:hover:not(.Mui-disabled, .Mui-error):before": {
+              borderBottomColor: colors.red,
+            },
+          }}
         />
+        <Spacer height={"16px"} />
         <CourseTable
           rows={visibleRows}
           totalRows={filteredRows.length}
