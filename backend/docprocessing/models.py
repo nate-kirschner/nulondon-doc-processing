79220deffa62fa4d9ac332e77f39d089b699d9cf
alinageng <geng.a@northeastern.edu>
2023-07-19 11:33:40 +0100

    DjangoFactory error

--- backend/docprocessing/models.py
+++ backend/docprocessing/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Course(models.Model):
     title = models.CharField(max_length=255)
     course_code = models.CharField(max_length=255)
