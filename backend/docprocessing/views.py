a6a7ee61396ba98f975376f5704bd1d1a1a0e456
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2023-07-20 10:18:37 +0100

    small change

--- backend/docprocessing/views.py
+++ backend/docprocessing/views.py
@@ -7,9 +7,10 @@ from django.core import serializers
 def courses(request):
     courses = Course.objects.all()
     ser_obj = serializers.serialize('json', courses)
-    response = HttpResponse(ser_obj, headers={
+    headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Origin',
         'Access-Control-Allow-Methods': 'GET, POST'
-    })
+    }
+    response = HttpResponse(ser_obj, headers=headers)
     return response
