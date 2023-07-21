f5303682aa7fdb0da772a77a9e8899b3d42fa428
aponakampalli <120080886+aponakampalli@users.noreply.github.com>
2023-07-21 12:15:05 +0100

    sample template

--- frontend/src/components/CreateTemplate.tsx
+++ frontend/src/components/CreateTemplate.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Box, Checkbox, FormControlLabel, FormGroup, Input, Typography } from "@mui/material";
+import React, { useState } from 'react';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Checkbox, FormControlLabel, FormGroup, Input, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { fontFamily, fontSize, fontWeight } from '@mui/system';
+import { colors } from '../theme';
+import AssessmentRow from './AssessmentRow';
 
 
 const CreateTemplate: React.FC = () => {
@@ -13,46 +15,240 @@ const CreateTemplate: React.FC = () => {
 
     return(
         <Box
-        sx={{
-            width: "80%"
-          }}
-            >
-            <Typography
-                textAlign="left"
-                sx={{ 
-                    paddingTop: "40px",
-                    fontSize: "24px",
-                    fontFamily: "lato",
-                    fontWeight: 700
-                 }}>
+        sx = {{
+            width: "90%",
+            paddingTop: "32px"
+        }}>
+        <React.Fragment
+        >
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                <TableHead>
+                <TableRow sx={{ backgroundColor: colors.gray }}>
+                <TableCell
+                sx={{ paddingX: "24px", fontSize: "20px", fontWeight: 700 }}
+                >
                 New Version
-            </Typography>
-            <Typography
-                textAlign="left"
-                sx={{ 
-                    paddingY: "20px",
-                    fontSize: "18px",
-                    fontFamily: "lato",
-                 }}>
-                Course Title: {courseDummy.title}<br/>
-                Assessment: {courseDummy.assessments[0].title}<br/>
-                Course Code: {courseDummy.code}<br/>
-                Version: {courseDummy.assessments[0].versions[2]}
-            </Typography>
-            <FormGroup>
-                <Typography
-                    textAlign="left"
-                    sx={{ 
-                    paddingY: "20px",
-                    fontSize: "18px",
-                    fontFamily: "lato",
-                 }}>
-                    Learning Objectives
-                </Typography>
-                <FormControlLabel control={<Checkbox defaultChecked />} label="LO1" />
-                <FormControlLabel control={<Checkbox />} label="LO2" />
-                <FormControlLabel control={<Checkbox />} label="LO3" />
-            </FormGroup>
+                </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              <React.Fragment>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Assessment Details
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        Assessment Content
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Assessment Tasks
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        Assessment Tasks Content
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Assessment Criteria
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        Assessment Criteria
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Marking
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        Marking Content
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Learning Outcomes
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        LO Content
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Assessing Feedback
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        Feedback Details
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Late Submissions
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        Submissions Content
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Extenuating Circumstances
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        Content
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Academic Misconduct
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        Content
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell colSpan={1}>
+                    <Accordion>
+                      <AccordionSummary>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            width: "100%",
+                          }}
+                        >
+                          <Typography sx={{ fontSize: "18px" }}>
+                            Version History
+                          </Typography>
+                        </Box>
+                      </AccordionSummary>
+                      <AccordionDetails>
+                        History Details
+                      </AccordionDetails>
+                    </Accordion>
+                  </TableCell>
+                </TableRow>
+              </React.Fragment>
+          </TableBody>
+        </Table>
+      </TableContainer>
+        </React.Fragment>
         </Box>
     );
 };
