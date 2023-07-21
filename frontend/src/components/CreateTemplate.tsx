c7b2b2781df2071eacb8cb5d23050bebfe0ba75b
aponakampalli <120080886+aponakampalli@users.noreply.github.com>
2023-07-21 10:57:29 +0100

    template text

--- frontend/src/components/CreateTemplate.tsx
+++ frontend/src/components/CreateTemplate.tsx
@@ -4,24 +4,40 @@ import { fontFamily, fontSize, fontWeight } from '@mui/system';
 
 
 const CreateTemplate: React.FC = () => {
+    const courseDummy = {
+        title: "Course 1",
+        code: "IS3500",
+        credits: 4,
+        assessments: [{ title: "Assessment 2", weighting: 50, versions: [1, 2, 3] }]
+    };
+
     return(
         <Box
         sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            width: "100%",
+            width: "80%"
           }}
             >
             <Typography
                 textAlign="left"
                 sx={{ 
-                    paddingY: "40px",
+                    paddingTop: "40px",
                     fontSize: "24px",
                     fontFamily: "lato",
-                    marginLeft: "80px",
                     fontWeight: 700
                  }}>
                 New Version
+            </Typography>
+            <Typography
+                textAlign="left"
+                sx={{ 
+                    paddingY: "20px",
+                    fontSize: "18px",
+                    fontFamily: "lato",
+                 }}>
+                Course Title: {courseDummy.title}<br/>
+                Assessment: {courseDummy.assessments[0].title}<br/>
+                Course Code: {courseDummy.code}<br/>
+                Version: {courseDummy.assessments[0].versions[2]}
             </Typography>
         </Box>
     );
