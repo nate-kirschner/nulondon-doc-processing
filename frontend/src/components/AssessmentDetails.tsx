ceb3b585d72f3fb4bc536dbcd3949ba889e5b973
aponakampalli <120080886+aponakampalli@users.noreply.github.com>
2023-07-24 12:33:23 +0100

    final assessment details

--- frontend/src/components/AssessmentDetails.tsx
+++ frontend/src/components/AssessmentDetails.tsx
@@ -1,110 +1,168 @@
-import { Center } from "@chakra-ui/react";
-import { Box, FormControl, FormControlLabel, InputLabel, MenuItem, Select, Switch, TextField, Typography } from "@mui/material";
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
-
+import { Center, theme } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  Select,
+  Switch,
+  Table,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { colors } from "../theme";
+import React from "react";
 
 const AssessmentDetails: React.FC = () => {
-    const courseDummy = {
-        title: "Course 1",
-        code: "IS3500",
-        credits: 4,
-        assessments: [{ title: "Assessment 2", weighting: 50, versions: [1, 2, 3] }]
-    };
-    return (
-        <Box>
-            <Typography
-                textAlign="left"
-                sx={{
-                    fontSize: "18px",
-                    fontFamily: "lato"
-                }}>
-                Course Title: {courseDummy.title}<br />
-                Course Code: {courseDummy.code}<br />
-                Course Leader: Mark Martin<br />
-                Level: Level 4
-                <br /><br />
-                Sitting: First Sitting<br />
-                Assessment: {courseDummy.assessments[0].title}<br />
-                Version: {courseDummy.assessments[0].versions[2]}<br />
-                Assessment Number: AE4
-                <br /><br />
-                <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        width: "80%",
-                    }}>
-                    <Box>
-                        Assessment Type:
-                        <TextField id="outlined-basic" label="" variant="outlined" size="small" /><br />
-                    </Box>
-                    <Box>
-                        Restrictions on Time/Length:
-                        <TextField id="outlined-basic" label="" variant="outlined" size="small" /><br />
-                    </Box>
-                </Box>
-                <Box
-                    sx={{
-                        display: "flex",
-                        width: "20%"
-                    }}>
-                    <Typography>
-                        Individual/Group:
-                    </Typography>
-                    <Select
-                        labelId="individual-group"
-                        id="individual-group"
-                        label=""
-                        size="small"
-                        fullWidth>
-                        <MenuItem value={"Individual"}>Individual</MenuItem>
-                        <MenuItem value={"Group"}>Group</MenuItem>
-                    </Select>
-                </Box>
-                Assessment Weighting: {courseDummy.assessments[0].weighting} %<br />
-                <Box>
-                    <Typography>
-                        Issue Date:
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <DatePicker />
-                        </LocalizationProvider>
-                    </Typography>
-                </Box><br />
-                Hand In Deadline: 
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker />
-                </LocalizationProvider>
-                <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        width: "90%",
-                    }}>
-                    <Typography>
-                        Planned Feedback Deadline:
-                        <TextField id="outlined-basic" label="" variant="outlined" size="small" /><br /><br />
-                    </Typography>
-                    <Typography>Mode of Submission:
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={""}
-                            label=""
-                            fullWidth
-                            size="small">
-                            <MenuItem value={"online"}>Online</MenuItem>
-                            <MenuItem value={"in-person"}>In person</MenuItem>
-                        </Select>
-                    </Typography>
-                </Box>
-                Anonymous Marketing:
-                <FormControlLabel control={<Switch defaultChecked />} label="" /><br />
-            </Typography>
-        </Box>
-    );
+  const courseDummy = {
+    title: "Course 1",
+    code: "IS3500",
+    credits: 4,
+    assessments: [
+      { title: "Assessment 2", weighting: 50, versions: [1, 2, 3] },
+    ],
+  };
+  const displayCourse = [
+    { displayName: "Course Title", displayValue: courseDummy.title },
+    { displayName: "Course Code", displayValue: courseDummy.code },
+    { displayName: "Course Leader", displayValue: "Mark Martin" },
+    { displayName: "Level", displayValue: "Level 4" },
+    { displayName: "Sitting", displayValue: "First Sitting" },
+    {
+      displayName: "Assessment",
+      displayValue: courseDummy.assessments[0].title,
+    },
+    {
+      displayName: "Version",
+      displayValue: courseDummy.assessments[0].versions[2],
+    },
+    { displayName: "Assessment Number", displayValue: "AE4" },
+    {
+      displayName: "Assessment Weighting",
+      displayValue: courseDummy.assessments[0].weighting + " %",
+    },
+  ];
+  return (
+    <React.Fragment>
+      <TableContainer>
+        <Table
+          sx={{
+            fontSize: "18px",
+            fontFamily: "lato",
+            "& .MuiOutlinedInput-root": {
+              "&:hover fieldset": {
+                borderColor: colors.red,
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: colors.red,
+              },
+            },
+          }}
+        >
+          {displayCourse.map((course) => {
+            return (
+              <TableRow>
+                <TableCell variant="head" width="80%" sx={{ fontWeight: "bold" }}>
+                  {course.displayName}
+                </TableCell>
+                <TableCell align="right" > {course.displayValue}</TableCell>
+              </TableRow>
+            );
+          })}
+          <TableRow>
+            <TableCell variant="head" sx={{ fontWeight: "bold" }}>
+              Assessment Type
+            </TableCell>
+            <TableCell align="right">
+              <TextField variant="outlined" size="small" fullWidth />
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell variant="head" sx={{ fontWeight: "bold" }}>
+              Restrictions on Time/Length
+            </TableCell>
+            <TableCell align="right">
+              <TextField variant="outlined" size="small" fullWidth/>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell variant="head" sx={{ fontWeight: "bold" }}>
+              Individual/Group
+            </TableCell>
+            <TableCell align="right">
+              <Select label="" size="small" fullWidth>
+                <MenuItem value={"Individual"}>Individual</MenuItem>
+                <MenuItem value={"Group"}>Group</MenuItem>
+              </Select>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell variant="head" sx={{ fontWeight: "bold" }}>
+              Issue Date
+            </TableCell>
+            <TableCell align="right">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker />
+              </LocalizationProvider>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell variant="head" sx={{ fontWeight: "bold" }}>
+              Hand In Deadline
+            </TableCell>
+            <TableCell align="right">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker />
+              </LocalizationProvider>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell variant="head" sx={{ fontWeight: "bold" }}>
+              Planned Feedback Deadline
+            </TableCell>
+            <TableCell align="right">
+              <TextField variant="outlined" size="small" fullWidth/>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell variant="head" sx={{ fontWeight: "bold" }}>
+              Mode of Submission
+            </TableCell>
+            <TableCell align="right">
+              <Select fullWidth size="small">
+                <MenuItem value={"online"}>Online</MenuItem>
+                <MenuItem value={"in-person"}>In person</MenuItem>
+              </Select>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell variant="head" sx={{ fontWeight: "bold" }}>
+              Anonymous Marketing
+            </TableCell>
+            <TableCell align="right">
+              <Switch
+              sx={{
+                "& .MuiSwitch-switchBase.Mui-checked": {
+                  color: colors.red
+                },
+                "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+                  backgroundColor: colors.red,
+                },
+              }}
+              />
+            </TableCell>
+          </TableRow>
+        </Table>
+      </TableContainer>
+    </React.Fragment>
+  );
 };
 
 export default AssessmentDetails;
