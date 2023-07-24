a7eced1b5402bd930c48759070d4d3a775f2623d
alinageng <geng.a@northeastern.edu>
2023-07-24 12:39:44 +0100

    use createHTTPResponse()

--- backend/docprocessing/views.py
+++ backend/docprocessing/views.py
@@ -20,30 +20,14 @@ def createHTTPResponse(object):
 
 def assessments(request, pk):
     assessment = get_object_or_404(Assessment, pk=pk)
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Origin',
-        'Access-Control-Allow-Methods': 'GET, POST'
-    }
 
     json_string = json.dumps(model_to_dict(assessment))
-    response = HttpResponse(json_string, headers=headers)
+    response = HttpResponse(json_string, headers=HEADERS)
     return response
 
 def learning_outcomes(request, course_code):
     learning_outcomes = LearningOutcomes.objects.filter(course_code=course_code)
-    ser_obj = serializers.serialize("json", learning_outcomes)
-
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Origin',
-        'Access-Control-Allow-Methods': 'GET, POST'
-    }
-
-    json_string = json.dumps(ser_obj)
-    response = HttpResponse(json_string, headers=headers)
-    return response
-
+    return createHTTPResponse(learning_outcomes)
 
 def courses(request):
     courses = Course.objects.all()
@@ -63,8 +47,6 @@ def courses(request):
         course_with_assessments['code'] = course.course_code
         course_with_assessments['assessments'] = course_assessments_list
         output.append(course_with_assessments)
-    
- 
 
     json_string = json.dumps(output)
     response = HttpResponse(json_string, headers=HEADERS)
