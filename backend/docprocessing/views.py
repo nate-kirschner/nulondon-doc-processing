646609c9d2cc6d58c738985051164d07ae9eda41
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2023-07-26 10:39:25 +0100

    update page size to 20

--- backend/docprocessing/views.py
+++ backend/docprocessing/views.py
@@ -52,6 +52,35 @@ def courses(request):
     response = HttpResponse(json_string, headers=HEADERS)
     return response
 
+def courses_paginated(request, page):
+    PAGE_SIZE = 20
+
+    number_of_courses = Course.objects.all().count()
+    if page < 1 or page > number_of_courses // PAGE_SIZE + 1:
+        return HttpResponse("Bad page number", status_code=400)
+
+    courses = Course.objects.all()[(page-1)*PAGE_SIZE:min(page*PAGE_SIZE, number_of_courses)]
+    output = []
+
+    for course in courses:
+        course_with_assessments = {}
+        course_assessments = Assessment.objects.filter(course_code=course.course_code)
+        course_assessments_list = list(course_assessments.values('id', 'activity'))
+
+        for assessment in course_assessments_list:
+            templates_for_assessment = Templates.objects.filter(course_code=course.course_code, 
+                                                              assessment_key=assessment['id'])
+            assessment['versions'] = list(templates_for_assessment.values_list('version', flat=True))
+
+        course_with_assessments['title'] = course.title
+        course_with_assessments['code'] = course.course_code
+        course_with_assessments['assessments'] = course_assessments_list
+        output.append(course_with_assessments)
+
+    json_string = json.dumps(output)
+    response = HttpResponse(json_string, headers=HEADERS)
+    return response
+
 
 def template(request, courseId, assessmentId, version):
     template = Templates.objects.filter(
