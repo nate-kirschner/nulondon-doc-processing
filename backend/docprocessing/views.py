115a65e16d8ee3e7a02e5e9eb2cbf49aec9886d5
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2023-07-26 10:31:41 +0100

    create paginated endpoint

--- backend/docprocessing/views.py
+++ backend/docprocessing/views.py
@@ -7,10 +7,10 @@ from django.core import serializers
 import json
 
 HEADERS = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Origin',
-        'Access-Control-Allow-Methods': 'GET, POST'
-    }
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Origin',
+    'Access-Control-Allow-Methods': 'GET, POST'
+}
 
 def createHTTPResponse(object):
     ser_obj = serializers.serialize('json', object)
@@ -31,6 +31,33 @@ def learning_outcomes(request, course_code):
 
 def courses(request):
     courses = Course.objects.all()
+    output = []
+
+    for course in courses:
+        course_with_assessments = {}
+        course_assessments = Assessment.objects.filter(course_code=course.course_code)
+        course_assessments_list = list(course_assessments.values('id', 'activity'))
+
+        for assessment in course_assessments_list:
+            templates_for_assessment = Templates.objects.filter(course_code=course.course_code, 
+                                                              assessment_key=assessment['id'])
+            assessment['versions'] = list(templates_for_assessment.values_list('version', flat=True))
+
+        course_with_assessments['title'] = course.title
+        course_with_assessments['code'] = course.course_code
+        course_with_assessments['assessments'] = course_assessments_list
+        output.append(course_with_assessments)
+
+    json_string = json.dumps(output)
+    response = HttpResponse(json_string, headers=HEADERS)
+    return response
+
+def courses_paginated(request, page):
+    number_of_courses = Course.objects.all().count()
+    if page < 1 or page > number_of_courses // 10 + 1:
+        return HttpResponse("Bad page number", status_code=400)
+
+    courses = Course.objects.all()[(page-1)*10:min(page*10, number_of_courses)]
     output = []
 
     for course in courses:
